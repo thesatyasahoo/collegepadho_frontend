c69a8e6f7294dc19bf202096545d8ce98ee84a19
satya sahoo <satyasahoo525@gmail.com>
2023-12-05 13:58:37 +0530

    fixed auth login

--- src/PrivateRoutes.js
+++ src/PrivateRoutes.js
@@ -1,55 +1,55 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Navigate, Outlet, useNavigate } from 'react-router-dom';
-import Loader from './components/Loader';
-import BASE_URL from './components/constant';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import Loader from "./components/Loader";
+import BASE_URL from "./components/constant";
 
 const PrivateRoutes = () => {
-    const navigate = useNavigate();
-  const [auth,setAuth]=useState(2);
-//   const [showLoader,setShowLoader]=useState(true);
+  const navigate = useNavigate();
+  const [auth, setAuth] = useState(2);
+  //   const [showLoader,setShowLoader]=useState(true);
   console.log(auth);
 
-    const checkLocalStorage =async ()=>{
-        const isValid = await JSON.parse(localStorage.getItem('IS_VALID'));
-        if(isValid==0){
-            setAuth(0);
-        }
-        if(isValid==1){
-            setAuth(1);
-        }
+  const checkLocalStorage = async () => {
+    const isValid = await JSON.parse(localStorage.getItem("IS_VALID"));
+    if (isValid == 0) {
+      setAuth(0);
     }
-  useEffect(()=>checkLocalStorage,[]);
-  const callApi = ()=>{
-    axios.get(BASE_URL+'/api/validate').then(async (res)=>{
-        console.log(res)
-      if(res.data.message=='Authenticated'){
-        await localStorage.setItem('IS_VALID',JSON.stringify(1));
-        setAuth(1);
-      }
-      if(res.data.message=='unauthorized'){
-        await localStorage.setItem('IS_VALID',JSON.stringify(0));
-        setAuth(0);
-      }
-    }).catch((err)=>{console.log(err)})
-  }
-  useEffect(callApi,[])
-  
-  if(auth==1){
-      return (
-        <Outlet/>
-      )
-  }
-  if(auth==0){
-    return (
-        <Navigate to="/admin/login"/>
-      )
-  }
-  if(auth==2){
-    return (
-        <Loader/>
-      )
-  }
-}
+    if (isValid == 1) {
+      setAuth(1);
+    }
+  };
+  useEffect(() => checkLocalStorage, []);
+  const callApi = () => {
+    let token = localStorage.getItem("token");
+    axios
+      .get(BASE_URL + `/api/validate?token=${token}`)
+      .then(async (res) => {
+        console.log(res);
+        if (res.data.message == "Authenticated") {
+          await localStorage.setItem("IS_VALID", JSON.stringify(1));
+          setAuth(1);
+        }
+        if (res.data.message == "unauthorized") {
+          await localStorage.setItem("IS_VALID", JSON.stringify(0));
+          setAuth(0);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(callApi, []);
 
-export default PrivateRoutes
+  if (auth == 1) {
+    return <Outlet />;
+  }
+  if (auth == 0) {
+    return <Navigate to="/admin/login" />;
+  }
+  if (auth == 2) {
+    return <Loader />;
+  }
+};
+
+export default PrivateRoutes;
--- src/components/admin/pages/LogInPage.jsx
+++ src/components/admin/pages/LogInPage.jsx
@@ -1,30 +1,35 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import BASE_URL from '../../constant';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import BASE_URL from "../../constant";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="">
         College Padho
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -35,53 +40,58 @@ const defaultTheme = createTheme({
   palette: {
     primary: {
       // Purple and green play nicely together.
-      main: '#ff9800',
+      main: "#ff9800",
     },
     secondary: {
       // This is green.A700 as hex.
-      main: '#ff9800',
+      main: "#ff9800",
     },
   },
 });
 
 export default function LogInPage() {
-    const navigate = useNavigate();
-    const [data,setData]= React.useState({
-        email:'',
-        password:'',
-    })
+  const navigate = useNavigate();
+  const [data, setData] = React.useState({
+    email: "",
+    password: "",
+  });
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(data);
-    axios.post(BASE_URL+'/api/verifyuser',data).then(async (res)=>{
+    axios
+      .post(BASE_URL + "/api/verifyuser", data)
+      .then(async (res) => {
         console.log(res);
-        if(res.data.message=="User Verified Successfully"){
-            const isValid = await localStorage.getItem('IS_VALID');
-            if(isValid==null || isValid==undefined || isValid=='undefined' || isValid=='null'){
-                await localStorage.setItem('IS_VALID',JSON.stringify(1));
-                navigate('/admin');
-            }
-            if(isValid==0){
-                await localStorage.setItem('IS_VALID',JSON.stringify(1));
-                navigate('/admin');
-            }
-            if(isValid==1){
-                await localStorage.setItem('IS_VALID',JSON.stringify(1));
-                navigate('/admin');
-            }
-            // await localStorage.setItem('IS_VALID',JSON.stringify(1));
-
+        localStorage.setItem("token", res?.data?.token);
+        if (res.data.message == "User Verified Successfully") {
+          const isValid = await localStorage.getItem("IS_VALID");
+          if (
+            isValid == null ||
+            isValid == undefined ||
+            isValid == "undefined" ||
+            isValid == "null"
+          ) {
+            await localStorage.setItem("IS_VALID", JSON.stringify(1));
+            navigate("/admin");
+          }
+          if (isValid == 0) {
+            await localStorage.setItem("IS_VALID", JSON.stringify(1));
+            navigate("/admin");
+          }
+          if (isValid == 1) {
+            await localStorage.setItem("IS_VALID", JSON.stringify(1));
+            navigate("/admin");
+          }
+          // await localStorage.setItem('IS_VALID',JSON.stringify(1));
+        } else {
+          alert(res.data.message);
         }
-        else{
-            alert(res.data.message);
-    }
-    }).catch((err)=>{
-      console.log(err);
-      alert('Something Went Wrong');
-    });
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Something Went Wrong");
+      });
   };
-
-  
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -90,18 +100,23 @@ export default function LogInPage() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
@@ -109,7 +124,9 @@ export default function LogInPage() {
               id="email"
               label="Email Address"
               name="email"
-              onChange={(e)=>{setData({...data,email:e.target.value})}}
+              onChange={(e) => {
+                setData({ ...data, email: e.target.value });
+              }}
             />
             <TextField
               margin="normal"
@@ -119,7 +136,9 @@ export default function LogInPage() {
               label="Password"
               type="password"
               id="password"
-              onChange={(e)=>{setData({...data,password:e.target.value})}}
+              onChange={(e) => {
+                setData({ ...data, password: e.target.value });
+              }}
             />
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
@@ -135,12 +154,16 @@ export default function LogInPage() {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2" sx={{color:'black'}}>
+                <Link href="#" variant="body2" sx={{ color: "black" }}>
                   Forgot password?
                 </Link>
               </Grid>
               <Grid item>
-                <span onClick={()=>{navigate('/admin/register')}}>
+                <span
+                  onClick={() => {
+                    navigate("/admin/register");
+                  }}
+                >
                   {"Don't have an account? Sign Up"}
                 </span>
               </Grid>
